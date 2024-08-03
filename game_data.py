a43f9e42607623fa3e9717cb253f2136fbe72f36
supriya lankotu <151497927+sweety113@users.noreply.github.com>
2024-08-03 11:53:25 +0530

    Add files via upload

---
+++ game_data.py
@@ -0,0 +1,130 @@
+data=[
+{
+        'name':"jenny's lectures",
+        'follower_count':100,
+        'description':"Youtuber",
+        'country':"india"
+},
+
+{
+        'name':"cristano ronanldo",
+        'follower_count':800,
+        'description':"foodball player",
+        'country':"portugal"
+},
+
+
+{
+        'name':"narendra modi",
+        'follower_count':700,
+        'description':"prime minister",
+        'country':"india"
+},
+
+
+{
+        'name':"supriya",
+        'follower_count':1000,
+        'description':"common girl",
+        'country':"india"
+},
+
+
+{
+        'name':"Fit tuber",
+        'follower_count':600,
+        'description':"Youtuber",
+        'country':"india"
+},
+
+
+{
+        'name':"selena gomez",
+        'follower_count':9000,
+        'description':"Musician and actress",
+        'country':"united states"
+},
+
+
+{
+        'name':"Kyle jennr",
+        'follower_count':200,
+        'description':" Reality Tv personality",
+        'country':"united states"
+},
+
+
+{
+        'name':"pankaj trapati",
+        'follower_count':650,
+        'description':"Actor",
+        'country':"india"
+},
+
+
+{
+        'name':"Lionel messi",
+        'follower_count':489,
+        'description':"footballer",
+        'country':"Argentina"
+},
+
+{
+        'name':"Nirej choopra",
+        'follower_count':489,
+        'description':"Athelete",
+        'country':"india"
+},
+
+{
+        'name':"T-series",
+        'follower_count':245,
+        'description':"Music label and Movie studio",
+        'country':"India"
+},
+
+{
+        'name':"Benyoce",
+        'follower_count':322,
+        'description':"Musician",
+        'country':"united states"
+},
+
+{
+        'name':"justin biber",
+        'follower_count':287,
+        'description':"Musician",
+        'country':"canada"
+},
+
+{
+        'name':"Talyor swift",
+        'follower_count':789,
+        'description':"Musician",
+        'country':"united states"
+},
+
+{
+        'name':"ISRO",
+        'follower_count':980,
+        'description':"space  agency",
+        'country':"India"
+},
+{
+        'name':"Mahendra singh dhoni",
+        'follower_count':990,
+        'description':" Cricketer",
+        'country':"India"
+},
+
+{
+        'name':"NASA",
+        'follower_count':780,
+        'description':"Space agency",
+        'country':"united states"
+},
+
+]
+
+
+
